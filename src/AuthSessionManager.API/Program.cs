7d27d203374df49470a834d639ee8e5b869fedb9
json-was <jason.wastavino@gmail.com>
2025-06-18 18:12:44 +1200

    ✨ feat: add Dockerfile and Docker Compose with PostgreSQL support

--- src/AuthSessionManager.API/Program.cs
+++ src/AuthSessionManager.API/Program.cs
@@ -17,7 +17,6 @@ builder.Services.AddSwaggerGen(options =>
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 
@@ -26,7 +25,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
